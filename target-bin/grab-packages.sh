26b7c274a05f90c3f7b8fb327a8a0507ed6303c2
Chris Kleeschulte <chrisk@bitpay.com>
2016-08-28 13:21:37 -0400

    removed the reinstall of deb files so that offline builds work.

--- target-bin/grab-packages.sh
+++ target-bin/grab-packages.sh
@@ -8,10 +8,6 @@ cd /var/cache/apt/archives
 
 # make sure all packages with installed versions are downloaded
 # (except for held packages, which may not be available for download)
-if [ -z $OFFLINE ]; then
-  dpkg-query -W -f '${Status}\t${Package}=${Version}\n' | grep -v ^hold | cut -f2- | xargs -n 50 apt-get install -q --reinstall -y -d > /tmp/download.log
-else
-  dpkg-query -W -f '${Status}\t${Package}=${Version}\n' | grep -v ^hold | cut -f2- > /tmp/download.log
-fi
+dpkg-query -W -f '${Status}\t${Package}=${Version}\n' | grep -v ^hold | cut -f2- > /tmp/download.log
 grep "cannot be downloaded" /tmp/download.log && { echo Could not download some packages, please run gbuild --upgrade 1>&2 ; exit 1 ; }
 sha256sum *.deb | sort --key 2
